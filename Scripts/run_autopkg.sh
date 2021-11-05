275fae83d441497a0a8e22706bda604b7ba4a29f
DJ Delisser <dj@djm1pro.local>
2021-11-05 10:36:32 -0400

    added run script

---
+++ Scripts/run_autopkg.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script will be setup to run on a scheduled basis via cron job
+# The schedule is below: 
+# Daily @ 8am EST
+# If new recipes are added, the recipe_list will need to be updated
+
+###Ensure ALL Repos are up to date
+autopkg repo-update all
+
+#Run the Recipe List
+#autopkg run --recipe-list /Volumes/JAMF/AutoPkg/RecipeRepos/com.github.djdelisser.jamf-recipes/recipe_list.plist
+
+#Test by running one singluar recipe run (-vvv for Very Verbose)
+autopkg run -vvv KiwiforGmail-pkg-upload.jamf.recipe.yaml
+
+exit 0 #Success
+exit 1 #Failure
