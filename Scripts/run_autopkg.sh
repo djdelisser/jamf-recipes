ce1083210c162d03b5d3361b86c6856d03723483
DJ Delisser <dj@djm1pro.local>
2021-11-05 18:14:15 -0400

    added run script

--- Scripts/run_autopkg.sh
+++ Scripts/run_autopkg.sh
@@ -6,13 +6,13 @@
 # If new recipes are added, the recipe_list will need to be updated
 
 ###Ensure ALL Repos are up to date
-/usr/local/bin/autopkg repo-update all
+autopkg repo-update all
 
 #Run the Recipe List
 #autopkg run --recipe-list /Volumes/JAMF/AutoPkg/RecipeRepos/com.github.djdelisser.jamf-recipes/recipe_list.plist
 
 #Test by running one singluar recipe run (-vvv for Very Verbose)
-/usr/local/bin/autopkg run -vvv KiwiforGmail-pkg-upload.jamf.recipe.yaml
+autopkg run -vvv KiwiforGmail-pkg-upload.jamf.recipe.yaml
 
 exit 0 #Success
 exit 1 #Failure
