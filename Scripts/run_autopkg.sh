1874081246dee83a35429a16a38f2e637d02f3dc
DJ Delisser <dj@djm1pro.local>
2021-11-08 08:19:12 -0500

    Script is up-to-date to run recipe_list instead of single recipe

--- Scripts/run_autopkg.sh
+++ Scripts/run_autopkg.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# This script will be setup to run on a scheduled basis via cron job
+# This script will be setup to run on a scheduled basis via cron job on the Litmus Cambridge Mac Mini
 # The schedule is below: 
-# Daily @ 8am EST
-# If new recipes are added, the recipe_list will need to be updated
+# Daily @ 8am EST, Monday - Friday, crontab below
+# 0 8 * * 1-5 /Volumes/JAMF/Autopkg/RecipeRepos/com.github.djdelisser.jamf-recipes/Scripts/run_autopkg.sh
+# Logs output to Log folder for Autopkg
+# NOTE: IF NEW RECIPES ARE ADDED, RECIPE_LIST WILL NEED TO BE UPDATED
 
-###Ensure ALL Repos are up to date
+# Ensure ALL Repos are up to date
 /usr/local/bin/autopkg repo-update all
 
 #Run the Recipe List
-#autopkg run --recipe-list /Volumes/JAMF/AutoPkg/RecipeRepos/com.github.djdelisser.jamf-recipes/recipe_list.plist
-
-#Test by running one singluar recipe run (-vvv for Very Verbose)
-/usr/local/bin/autopkg run -vvv KiwiforGmail-pkg-upload.jamf.recipe.yaml
+autopkg run --recipe-list /Volumes/JAMF/AutoPkg/RecipeRepos/com.github.djdelisser.jamf-recipes/recipe_list.plist
 
 exit 0 #Success
 exit 1 #Failure
